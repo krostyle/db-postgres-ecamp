ea4c69f3080f4a52bd93f80fbc85c3fa4c202839
Krostyle <diegoo.albarran@gmail.com>
2021-07-07 18:44:55 -0400

    Se finaliza Parte 2

--- Posts.sql
+++ Posts.sql
@@ -57,3 +57,75 @@ INSERT INTO posts(
 
 
 /*PARTE 2*/
+
+CREATE TABLE comentarios
+(
+    id integer NOT NULL,
+    fecha date NOT NULL,
+    hora_creacion time  NOT NULL,
+    contenido varchar(255) NOT NULL,
+	id_post integer NOT NULL,
+    CONSTRAINT pk_id_comentarios PRIMARY KEY (id)
+);
+
+ALTER TABLE comentarios
+	ADD CONSTRAINT fk_comentarios_posts
+	FOREIGN KEY (id_post)
+	REFERENCES posts(id)
+	
+
+
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (1, current_date, current_time,'Comentario 1 de Pamela' , 1);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (2, current_date, current_time,'Comentario 2 de Pamela' , 1);
+
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (3, current_date, current_time,'Comentario 1 de Carlos' , 6);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (4, current_date, current_time,'Comentario 2 de Carlos' , 6);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (5, current_date, current_time,'Comentario 3 de Carlos' , 6);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (6, current_date, current_time,'Comentario 4 de Carlos' , 6);
+
+
+INSERT INTO posts(
+	id, nombre_usuario, fecha_creacion, contenido, descripcion,titulo)
+	VALUES (7, 'Margarita', current_date, 'Post 1', 'Este es el primero post de Margarita','Titulo Post 1 Margarita')
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (7, current_date, current_time,'Comentario 1 de Margarita' , 7);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (8, current_date, current_time,'Comentario 2 de Margarita' , 7);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (9, current_date, current_time,'Comentario 3 de Margarita' , 7);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (10, current_date, current_time,'Comentario 4 de Margarita' , 7);
+	
+INSERT INTO comentarios(
+	id, fecha, hora_creacion, contenido, id_post)
+	VALUES (11, current_date, current_time,'Comentario 5 de Margarita' , 7);
+
+SELECT * FROM posts JOIN comentarios ON posts.id=comentarios.id_post;
+
+SELECT * FROM posts;
+
+SELECT * FROM comentarios;
